1d40a0df4dc6293cf6ed2326f191e0fd8f8191ea
Carlos Balcazar <cbalcazarp@gmail.com>
2021-08-28 15:48:27 -0500

    code

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace upn_T1_q_6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Decimal salario, salario1;
+            Console.WriteLine("Cantidad de billetes de de 10,20,50,100 y 200 nuevos soles recibirá un empleado como pago por su trabajo");
+            Console.Write("Ingrese salario: ");
+            salario = Convert.ToDecimal(Console.ReadLine());
+            salario1 = Math.Truncate(salario);
+            Console.WriteLine(salario1);
+            
         }
     }
 }
